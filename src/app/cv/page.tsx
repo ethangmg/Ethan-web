fe32dc9783a16bd6f8afbc6a3eea395cc0b9007f
Ethan <ethangmg@gmail.com>
2024-07-18 21:00:00 -0600

    task/create-skills

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 
 export default function CV() {
   return (
-    <div className='h-screen bg-gray-950 bg-gradient-second border-2 border-green-500 flex flex-col content-center items-center justify-center'>
-      <div className='bg-gradient-blackCherry h-96 w-64 mb-16 rounded-lg text-center items-center content-center'>
-        <h1 className='text-center text-white text-sm font-bold'>Welcome to the CV Page</h1>
-        <p className='text-center text-white text-lg'>This is the default CV page.</p>
-      </div>
+    <div className='h-screen bg-gray-950 bg-gradient-reverse-blackCherry border-2 border-green-500 flex flex-col content-center items-center justify-center'>
+      
     </div>
   )
 }
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,10 +2,47 @@ import React from 'react'
 
 export default function Skills() {
   return (
-    <div className='h-screen bg-gray-950 bg-gradient-second border-2 border-green-500 flex flex-col content-center items-center justify-center'>
-      <div className='bg-gradient-blackCherry h-96 w-64 mb-16 rounded-lg text-center items-center content-center'>
-        <h1 className='text-center text-white text-sm font-bold'>Welcome to the Skills Page</h1>
-        <p className='text-center text-white text-lg'>This is the default Skills page.</p>
+    <div className='h-screen bg-gradient-reverse-blackCherry flex flex-col items-center '>
+      <div className='m-5 text-2xl flex flex-row w-screen'>
+        <div className='border-2 border-l-0 border-white w-1/2 flex flex-row content-end justify-end text-end pr-2 rounded-r-lg'>
+          <h1>Skills</h1>
+        </div>
+      </div>
+      <div className='border-b-2 border-white w-screen px-2 text-sm'>
+        <ul className='h-48 flex flex-wrap'>
+          <li className='mx-2'>-React </li>
+          <li className='mx-2'>-JavaScript</li>
+          <li className='mx-2'>-CSS </li>
+          <li className='mx-2'>-HTML </li>
+          <li className='mx-2'>-Tailwind CSS </li>
+          <li className='mx-2'>-Redux Toolkit </li>
+          <li className='mx-2'>-Typescript </li>
+          <li className='mx-2'>-React Hooks </li>
+          <li className='mx-2'>-react-router </li>
+          <li className='mx-2'>-react-hook-form </li>
+          <li className='mx-2'>-Formik </li>
+          <li className='mx-2'>-React Native Expo </li>
+          <li className='mx-2'>-Git & Github </li>
+          <li className='mx-2'>-NPM </li>
+        </ul>
+      </div>
+      <div className='text-lg p-4'>
+        <p>Also during this 4 years since 2020 i got other soft skills as:</p>
+      </div>
+      <div className='text-sm'>
+        <ul>
+          <li>-Good communication</li>
+          <li>-Feedback every day</li>
+          <li>-Always wanting to learn more</li>
+          <li>-Resilience</li>
+          <li>-Patience</li>
+          <li>-Problem-solving skills</li>
+          <li>-Adaptability</li>
+          <li>-Self-motivation</li>
+          <li>-Teamwork</li>
+          <li>-Creative thinking</li>
+          <li>-Time management</li>
+        </ul>
       </div>
     </div>
   )
