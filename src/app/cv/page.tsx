2210501c01ad3dc70d355733438cdf5fe2f0cbf8
Ethan <ethangmg@gmail.com>
2024-07-22 13:54:54 -0600

    .

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,15 +1,17 @@
+"use client"
+
 import React from 'react'
 
+
+
+
 export default function CV() {
+  const rute = 'file:///C:/Users/ethan/Documents/doc.pdf'
   return (
     <div className='h-screen bg-gray-950 bg-gradient-reverse-blackCherry border-2 border-green-500 flex flex-col content-center items-center justify-center'>
-
-      
-
-      <h1>
-        Hola Mundo
-      </h1>
-
+      <h1>PDF</h1>
+      <div>          
+      </div>
     </div>
   )
 }
