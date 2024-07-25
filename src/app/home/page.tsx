1d19336272e255a8ac57f5457cb3bf69f4dada6a
Ethan <ethangmg@gmail.com>
2024-07-24 20:46:28 -0600

    task/create-HomeResponsive

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -11,11 +11,11 @@ import { GiSkills } from "react-icons/gi";
 export default function Home() {
 
   return (
-    <div className=' h-screen w-full bg-gray-950 bg-gradient-teal flex flex-row justify-center content-center items-center text-white'>
-      <div id='0' className='hover:z-10 bg-gradient-reverse-blackCherry h-screen w-48 rounded-lg flex flex-col text-center items-center justify-center content-center transition ease-in-out delay-150  hover:-translate-x-0 hover:-translate-y-14 hover:absolute hover:w-96 hover:h-5/6 hover:scale-75 hover:shadow-3xl'>
+    <div className=' h-full w-full bg-gray-950 bg-gradient-teal flex flex-col items-center text-white '>
+      <div id='0' className='bg-gradient-reverse-blackCherry h-screen w-screen rounded-lg flex flex-col text-center items-center justify-center content-center my-5 border-2 border-white sm:w-full'>
         <div className=''>
           <Image
-            className='rounded-full my-5 w-52 h-52 hover:w-72 hover:h-72'
+            className='rounded-full my-5 w-52 h-52 '
             src={Image1}
             alt='Sample Image'
           />
@@ -42,7 +42,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div id='1' className=' hover:z-0 bg-transparent backdrop-blur-lg h-screen w-48 rounded-lg flex flex-col text-center items-center justify-evenly content-center transition ease-in-out delay-150 hover:-translate-x-0 hover:-translate-y-14 hover:absolute hover:w-11/12 hover:h-screen hover:scale-75 hover:shadow-3xl hover:px-5'>
+      <div id='1' className=' bg-transparent backdrop-blur-lg h-screen w-screen rounded-lg flex flex-col text-center items-center justify-evenly content-center mb-5 p-5 sm:w-1/2 border-2 border-white  sm:w-full'>
         <div className='border-b-2 border-white border-double h-max '>
           <div className='text-start mb-2 text-slate-200'>
             <h1 className={`text-2xl`}>Resume</h1>
@@ -57,20 +57,20 @@ export default function Home() {
           </div>
         </div>
         <div className='w-full mb-10 flex flex-col items-center'>
-          <div className='flex flex-row items-center content-center justify-stretch text-2xl my-5 px-5  w-full'>
+          <div className='flex flex-row items-center content-center justify-stretch text-2xl my-5 px-5  w-full '>
             <GiSkills />
             <h2>Skills</h2>
           </div>
           <div className='text-xl text-start '>
-            <ul>
-              <li>-HTML</li>
-              <li>-CSS</li>
-              <li>-Java Script</li>
-              <li>-React JS</li>
-              <li>-TailwindCSS</li>
-              <li>-Redux toolkit</li>
-              <li>-Typescript</li>
-              <li>-React Native</li>
+            <ul className='sm:flex sm:flex-row sm:justify-center sm:w-screen sm:flex-wrap sm:px-2'>
+              <li className='sm:m-2'>-HTML</li>
+              <li className='sm:m-2'>-CSS</li>
+              <li className='sm:m-2'>-Java Script</li>
+              <li className='sm:m-2'>-React JS</li>
+              <li className='sm:m-2'>-TailwindCSS</li>
+              <li className='sm:m-2'>-Redux toolkit</li>
+              <li className='sm:m-2'>-Typescript</li>
+              <li className='sm:m-2'>-React Native</li>
             </ul>
           </div>
         </div>
