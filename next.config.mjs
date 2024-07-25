b38f9ecfbfa58af2b397965fa18297c49069d176
Ethan <ethangmg@gmail.com>
2024-07-24 19:59:42 -0600

    task/create-CV

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-      appDir: true
-    },
-    reactStrictMode: true
-  }
-  
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["img.freepik.com", "veterinaire-tour-hassan.com"],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+  reactStrictMode: true,
+};
+
 export default nextConfig;
--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,16 +1,24 @@
 "use client"
-
 import React from 'react'
-
-
+import CVImage from '../assets/Screenshot 2024-07-24 184714.png'
+import Image from 'next/image'
 
 
 export default function CV() {
-  const rute = 'file:///C:/Users/ethan/Documents/doc.pdf'
+
+
   return (
-    <div className='h-screen bg-gray-950 bg-gradient-reverse-blackCherry border-2 border-green-500 flex flex-col content-center items-center justify-center'>
-      <h1>PDF</h1>
-      <div>          
+    <div className='min-h-screen bg-gray-950 bg-gradient-reverse-blackCherry flex flex-col content-center items-center '>
+      <h1 className='w-screen border-b-2 border-white text-center m-5'>My Resume</h1>
+      <div className='p-5'>
+        <a href='https://www.linkedin.com/in/ethan-mart%C3%ADnez-553a9b289/overlay/1721868286870/single-media-viewer/?profileId=ACoAAEYbLk8BIXuoGbmL3DPMjvSAa7RoFSz3ZsQ'>
+          <Image
+            src={CVImage}
+            alt='CV'
+            height={500}
+            width={500}
+          />
+        </a>
       </div>
     </div>
   )
